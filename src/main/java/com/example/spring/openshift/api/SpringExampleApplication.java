cdc655eedfc4376c7216dae4c083d2bdbbce2ba2
martinzzler99 <89845738+martinzzler99@users.noreply.github.com>
2021-08-31 13:50:26 +0200

    Add files via upload

--- src/main/java/com/example/spring/openshift/api/SpringExampleApplication.java
+++ src/main/java/com/example/spring/openshift/api/SpringExampleApplication.java
@@ -2,6 +2,9 @@ package com.example.spring.openshift.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
